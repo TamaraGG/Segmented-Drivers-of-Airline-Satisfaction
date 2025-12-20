76312e1dc58d2ff5d50cdb943a9fc91b39a5f779
TamaraGG <goncharova2804t@gmail.com>
2025-12-20 15:32:15 +0300

    added class for main pipeline for one segment

---
+++ src/pipeline.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.interpretation import ShapAnalyzer
+import src.config as cfg
+
+
+class TrainingPipeline:
+    """
+    Connects SegmentManager, Trainer, Evaluator и Deployer.
+    """
+
+    def __init__(self, trainer, evaluator, deployer):
+        self.trainer = trainer
+        self.evaluator = evaluator
+        self.deployer = deployer
+
+    def run_segment(self, data: dict) -> dict:
+        """
+        Executes the whole cycle for one segment
+        """
+        seg_name = data['name']
+        X_train, y_train = data['X_train'], data['y_train']
+        X_test, y_test = data['X_test'], data['y_test']
+
+        print(f"\n{'=' * 10} Processing: {seg_name} {'=' * 10}")
+
+        # 1. train
+        best_model = self.trainer.train(
+            X_train, y_train,
+            param_grid=cfg.XGB_PARAM_GRID,
+            scoring='f1'
+        )
+        self.deployer.save_model(best_model, seg_name)
+
+        # 2. evaluate
+        metrics = {}
+        if X_test is not None:
+            y_pred = best_model.predict(X_test)
+            y_proba = best_model.predict_proba(X_test)[:, 1]
+
+            metrics = self.evaluator.calculate_metrics(y_test, y_pred, y_proba)
+            self.deployer.save_metrics(metrics, seg_name)
+
+            print(f"   Metrics: {metrics}")
+
+        # 3. interpretation and plots
+        self._run_interpretation(best_model, X_train, X_test, seg_name)
+
+        return metrics
+
+    def _run_interpretation(self, model, X_train, X_test, seg_name):
+        """
+        Builds and saves plots.
+        """
+        if X_test is None: return
+
+        analyzer = ShapAnalyzer(model, X_train)
+        print(f"   Generating plots for {seg_name}...")
+
+        # A. Summary Plot
+        analyzer.plot_summary(X_test, max_display=10)
+        self.deployer.save_plot(f"SHAP_Summary_{seg_name}")
+        plt.show()
+
+        # B. Saturation Analysis (Top-3 features)
+        top_df = analyzer.get_top_features(X_test, top_n=3)
+        top_feats = top_df['Feature'].tolist()
+
+        for feat in top_feats:
+            if X_train[feat].nunique() > 2:
+                analyzer.plot_dependence(X_test, feature=feat)
+                self.deployer.save_plot(f"SHAP_Dependence_{seg_name}_{feat}")
+                plt.show()
+
+        # C. Delay Interaction
+        if cfg.DELAY_OUTPUT_COL in X_test.columns:
+            top_service = top_feats[0]
+            analyzer.plot_interaction(
+                X_test,
+                feature_x=cfg.DELAY_OUTPUT_COL,
+                feature_color=top_service
+            )
+            self.deployer.save_plot(f"SHAP_Interaction_{seg_name}")
+            plt.show()
